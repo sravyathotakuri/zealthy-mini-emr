35aa37a1e9380324f3c1599bb020bf2fd0044c8e
Sravya Thotakuri <sravyathotakuri13579@gmail.com>
2025-10-27 17:40:08 -0400

    Remove old migrations

--- prisma/migrations/20251007203808_init_or_add_med_catalog/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "MedicationCatalog" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "medicationName" TEXT NOT NULL,
-    "dosage" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "MedicationCatalog_medicationName_dosage_key" ON "MedicationCatalog"("medicationName", "dosage");
--- prisma/migrations/20251013005556_drop_usersession/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Appointment" ADD COLUMN "provider" TEXT;
-ALTER TABLE "Appointment" ADD COLUMN "repeatSchedule" TEXT;
-ALTER TABLE "Appointment" ADD COLUMN "repeatUntil" DATETIME;
-
--- AlterTable
-ALTER TABLE "Prescription" ADD COLUMN "quantity" INTEGER;
-ALTER TABLE "Prescription" ADD COLUMN "refillDate" DATETIME;
-ALTER TABLE "Prescription" ADD COLUMN "refillSchedule" TEXT;
